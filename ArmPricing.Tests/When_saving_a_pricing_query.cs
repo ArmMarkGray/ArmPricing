6301d280aaa9109e58b3021b99c695a05f4df25c
info@millbank <in@magsolutionslimited.co.uk>
2015-09-29 19:27:53 +0100

    Adding adding first red test

---
+++ ArmPricing.Tests/When_saving_a_pricing_query.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Pricing.Controllers;
+
+namespace ArmPricing.Tests
+{
+    [TestFixture]
+    public class When_saving_a_pricing_query
+    {
+        private PricingQueryController controllerUnderTest;
+
+        [SetUp]
+        public void SetUpTests()
+        {
+            controllerUnderTest = new PricingQueryController();
+        }
+
+        [Test]
+        public void Should_MakeACallToThePricingQueryService_When_AttemptingToRegisterAPricingQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ArmPricing/Controllers/PricingQueryController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Pricing.Controllers
+{
+    public class PricingQueryController : Controller
+    {
+        //
+        // GET: /PricingQuery/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
