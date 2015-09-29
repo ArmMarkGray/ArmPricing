c3953ec461a990a39cb26645ebc6de335a161917
info@millbank <in@magsolutionslimited.co.uk>
2015-09-29 20:04:42 +0100

    First refactor as the code was repeating itself too much

--- ArmPricing.Tests/When_saving_a_pricing_query.cs
+++ ArmPricing.Tests/When_saving_a_pricing_query.cs
@@ -16,6 +16,7 @@ namespace ArmPricing.Tests
         private IPricingQueryService _pricingQueryServiceMock;
         private IEmailingService _emailingServiceMock;
         private IMapCustomerPricingModels _customerMapper;
+        private ViewResult _result;
 
         [SetUp]
         public void SetUpTests()
@@ -23,35 +24,30 @@ namespace ArmPricing.Tests
             _pricingQueryServiceMock = MockRepository.GenerateMock<IPricingQueryService>();
             _emailingServiceMock = MockRepository.GenerateMock<IEmailingService>();
             _controllerUnderTest = new PricingQueryController(_pricingQueryServiceMock, _emailingServiceMock);
+            _result = _controllerUnderTest.Register() as ViewResult;
         }
 
         [Test]
         public void Should_MakeACallToThePricingQueryService_When_AttemptingToRegisterAPricingQuery()
         {
-            _controllerUnderTest.Register();
             _pricingQueryServiceMock.AssertWasCalled(x => x.RegisterPricingQuery());
         }
 
         [Test]
         public void Should_MakeACallToTheEmailService_When_RegisteringAPricingQuery()
         {
-            _controllerUnderTest.Register();
             _emailingServiceMock.AssertWasCalled(x => x.SendEmailToTheSalesTeam());
         }
 
         [Test]
         public void Should_SendTheCustomerToTheThankYouPage_When_ThePricingQueryHasBeenRegistered()
         {
-            var result = _controllerUnderTest.Register() as ViewResult;
-
-            Assert.That(result.ViewName, Is.EqualTo("ThankYou"));
+            Assert.That(_result.ViewName, Is.EqualTo("ThankYou"));
         }
 
         [Test]
         public void Should_MapACustomerPricingQueryModelWithCustomerLastName_When_AttemptingToRegisterAPricingQuery()
         {
-            _controllerUnderTest.Register();
-
             _customerMapper.AssertWasCalled(x=>x.Map(Arg<CustomerPricingModel>.Is.Anything));
         }
     }
